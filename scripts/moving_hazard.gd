4bd83c083eeebd77dd3598c84a00a3096a5a9c29
Joshua Cory <accounts@keeplooking.net>
2024-04-08 09:41:05 -0600

    created Moving Hazard

---
+++ scripts/moving_hazard.gd
@@ -0,0 +1,47 @@
+extends AnimatableBody2D
+
+@onready var player = get_tree().get_first_node_in_group("Player")
+
+## the end point of travel path
+@export var destination: Vector2
+## the amount  of time it takes to travel between beginning and end points
+@export var duration: float = 1.0
+## waite time at start point
+@export var wait_time_1: float = 0.0
+## waite time at end point
+@export var wait_time_2: float = 0.0
+
+
+func _ready() -> void:
+	var tween = create_tween()
+	tween.set_loops()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.tween_interval(wait_time_1)
+	tween.tween_property(self, "global_position", global_position + destination, duration)
+	tween.tween_interval(wait_time_2)
+	tween.tween_property(self, "global_position", global_position, duration)
+	
+
+
+func _on_area_bounce_up_body_entered(body):
+	if body != null:
+		body.bounce_up()
+		body.is_bounceing = true
+
+
+func _on_area_bounce_down_body_entered(body):
+	if body != null:
+		body.bounce_down()
+		body.is_bounceing = true
+
+
+func _on_area_bounce_left_body_entered(body):
+	if body != null:
+		body.bounce_left()
+		body.is_bounceing = true
+
+
+func _on_area_bounce_right_body_entered(body):
+	if body != null:
+		body.bounce_right()
+		body.is_bounceing = true
