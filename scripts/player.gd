f28ef6ab7e3d2708d940d397024ad07f69666eb7
Joshua Cory <accounts@keeplooking.net>
2024-05-07 16:53:30 -0600

    made level 13

--- scripts/player.gd
+++ scripts/player.gd
@@ -124,7 +124,7 @@ func die():
 		
 func _on_no_bounce():
 	if no_bounce == true:
-		await get_tree().create_timer(.2).timeout
+		await get_tree().create_timer(.5).timeout
 		if no_bounce == true:
 			die()
 	else:
