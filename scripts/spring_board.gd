ebc6e73169874dae7ec55ebbdc5e63dd6da73d54
Joshua Cory <accounts@keeplooking.net>
2024-08-16 17:38:21 -0600

    made level 44

---
+++ scripts/spring_board.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+##min bounce force amount of player bounce
+@export var bounce_force_min: int = 500
+##max bounce force amount of player bounce
+@export var bounce_force_max: int = 650
+#sprite rfrence
+@onready var sprite = $AnimatedSprite2D
+
+func _on_body_entered(body):
+	if body != null:
+		sprite.play("default")
+		if body.gravity > 0:
+			body.bounce_up(bounce_force_min, bounce_force_max)
+			if body.pause_y:
+				body.pause_y = false
+			if body.stop_contorls:
+				body.stop_contorls = false
+		elif body.gravity < 0:
+			body.bounce_down(bounce_force_min, bounce_force_max)
+			if body.pause_y:
+				body.pause_y = false
+			if body.stop_contorls:
+				body.stop_contorls = false
