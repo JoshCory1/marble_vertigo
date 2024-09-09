f6f0e5ddbb5c5317ad6273d5b8f0afb544d86c87
Joshua Cory <accounts@keeplooking.net>
2024-09-09 14:06:10 -0600

    made level 57

--- scripts/moving_platform.gd
+++ scripts/moving_platform.gd
@@ -25,7 +25,7 @@ func _ready():
 func _on_body_entered(body):
 	if body != null:
 		if body.gravity > 0:
-			body.bounce_up(bounce_force_min / 2, bounce_force_max / 2)
+			body.bounce_up(round(bounce_force_min / 2),round(bounce_force_max / 2))
 			if body.pause_y:
 				body.pause_y = false
 			if body.stop_contorls:
