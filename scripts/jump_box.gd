ee57b917402fafc863530cc9965b2ab939bb7c35
Joshua Cory <accounts@keeplooking.net>
2024-09-11 19:34:17 -0600

    made level 59 level debug 3 and 4

--- scripts/jump_box.gd
+++ scripts/jump_box.gd
@@ -6,7 +6,7 @@ extends StaticBody2D
 @export var bounce_force_max: int = 450
 
 func _on_top_body_entered(body):
-	if body != null:
+	if body:
 		body.bounce_up(bounce_force_min, bounce_force_max)
 		if body.pause_y:
 			body.pause_y = false
@@ -15,7 +15,7 @@ func _on_top_body_entered(body):
 
 
 func _on_bottom_body_entered(body):
-	if body != null:
+	if body:
 		body.bounce_down(bounce_force_min, bounce_force_max)
 		if body.pause_y:
 			body.pause_y = false
@@ -23,7 +23,7 @@ func _on_bottom_body_entered(body):
 			body.stop_contorls = false
 
 func _on_right_body_entered(body):
-	if body != null:
+	if body:
 		body.bounce_right(bounce_force_min, bounce_force_max)
 		if body.pause_y:
 			body.pause_y = false
@@ -32,7 +32,7 @@ func _on_right_body_entered(body):
 
 
 func _on_left_body_entered(body):
-	if body != null:
+	if body:
 		body.bounce_left(bounce_force_min, bounce_force_max)
 		if body.pause_y:
 			body.pause_y = false
