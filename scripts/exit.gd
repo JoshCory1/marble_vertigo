c0fa42879459ad2286ef690b09d8a759f4488e5f
Joshua Cory <accounts@keeplooking.net>
2024-11-23 22:12:36 -0700

    debug level 58 and added vortex / gray screen reader shaders

--- scripts/exit.gd
+++ scripts/exit.gd
@@ -9,13 +9,14 @@ extends Area2D
 ##waite time at end point
 @export var wait_time_2: float = 0.0
 ##time untill player exits level
-@export var time_till_exit = 0.8
+@export var time_till_exit = .0
 ##the current level before level += 1
 @export var current_lvl = 1
 ##amount to increase current level
 @export var amount_to_increase: int = 1
-#refrence to the level
+#onready vars
 @onready var level = $".."
+@onready var animaytion_player = $AnimationPlayer
 
 func _ready():
 	var tween = create_tween()
@@ -27,16 +28,15 @@ func _ready():
 	tween.tween_property(self, "global_position", global_position, duration)
 
 func _on_body_entered(body):
+	animaytion_player.play("vortex")
 	if GameController.current_lvl <= current_lvl:
 		GameController.current_lvl = GameController.current_lvl + amount_to_increase
 	GameController.coins += level.coins_this_level
-	GameController.save_game()
-
-	AudioPlayer.play_sfx("portal_sfx")
-
 	body.stop_velocity = true
 	body.velocity = Vector2(0,0)
 	body.sprite.visible = false
-	await get_tree().create_timer(time_till_exit).timeout
+	AudioPlayer.play_sfx("portal_sfx")
+	GameController.save_game()
 	GameController.my_log("Player enterd")
+	await animaytion_player.animation_finished
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
